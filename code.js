e6b06c0283a2d59d26bc8f2deb0186cb4ff58132
SusanHex <suehex4027@gmail.com>
2024-01-22 16:30:20 -0700

    add query string function from gist.github.com/tanaikech/70503e0ea6998083fcb05c6d2a857107

--- code.js
+++ code.js
@@ -2,6 +2,8 @@ const API_ROOT = "https://api.wanikani.com/v2/";
 const SUBJECTS_URL = API_ROOT + "subjects";
 const REVIEWS_URL  = API_ROOT + "reviews";
 
+String.prototype.addQuery = function (obj) {return this + "?" + Object.entries(obj).flatMap(([k, v]) => Array.isArray(v) ? v.map(e => `${k}=${encodeURIComponent(e)}`) : `${k}=${encodeURIComponent(v)}`).join("&");};
+
 function main () {
   
 }
