75763b4f875b5a6675807c1a62569a8d050067d5
SusanHex <suehex4027@gmail.com>
2024-01-22 16:19:13 -0700

    Add constants and get_json function stub

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
+const API_ROOT = "https://api.wanikani.com/v2/";
+const SUBJECTS_URL = API_ROOT + "subjects";
+const REVIEWS_URL  = API_ROOT + "reviews";
+
 function main () {
   
 }
+
+function get_json (url, query=null) {
+
+} 
