b13230eaa449f2c82ff46002873550d2de47709e
SusanHex <suehex4027@gmail.com>
2024-01-23 13:33:14 -0700

    Add get_json function body and API_TOKEN constant

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 const API_ROOT = "https://api.wanikani.com/v2/";
 const SUBJECTS_URL = API_ROOT + "subjects";
 const REVIEWS_URL  = API_ROOT + "reviews";
-
+const API_TOKEN = "";
 String.prototype.addQuery = function (obj) {return this + "?" + Object.entries(obj).flatMap(([k, v]) => Array.isArray(v) ? v.map(e => `${k}=${encodeURIComponent(e)}`) : `${k}=${encodeURIComponent(v)}`).join("&");};
 
 function main () {
@@ -9,5 +9,23 @@ function main () {
 }
 
 function get_json (url, query=null) {
-
+    let url_string = url; 
+    let config = {
+      'method': 'get',
+      'headers': {
+        'Authorization': API_TOKEN, 
+      },
+    };
+    if (query !== null) {
+        url_string = url_string.addQuery(query);
+    };
+    try {
+        let response = UrlFetchApp.fetch(url_string, config);
+        if (response.getResponseCode() === 200) {
+            return JSON.parse(response.getContentText());
+        }
+    }
+    catch (e){
+        throw e;
+    }
 } 
